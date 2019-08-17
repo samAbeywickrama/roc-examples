8cd4a7e32c527e3528f57b454ad146a5d94b8280
Sameera Abeywickrama <abeywickrama.sam@gmail.com>
2019-08-17 13:26:45 +0530

    Updates

---
+++ precache-manifest.5c63c18d8f0dec6016dca3cfb19524be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40a46075b022fe249a5f210067fb6f7",
+    "url": "/roc-examples/index.html"
+  },
+  {
+    "revision": "a5261093aa5d60b7b777",
+    "url": "/roc-examples/static/css/main.d7416d77.chunk.css"
+  },
+  {
+    "revision": "fba60dfe58f448a8fbf3",
+    "url": "/roc-examples/static/js/2.acc348f4.chunk.js"
+  },
+  {
+    "revision": "a5261093aa5d60b7b777",
+    "url": "/roc-examples/static/js/main.22c913be.chunk.js"
+  },
+  {
+    "revision": "6b0bfa7a972f865537aa",
+    "url": "/roc-examples/static/js/runtime~main.5bc91ea4.js"
+  }
+]);
